9618800f089ee8c347951bf65bed1bd656ab8f7f
Kevin Palm <kevinpalm24@gmail.com>
2016-08-15 15:24:05 -0700

    implimented simpleload() in utilities

---
+++ code/explore.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from utilities import simpleload
+
+simpleload()
--- code/utilities.py
+++ code/utilities.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
+def simpleload():
+
+    # Read in the data
+    people = pd.read_csv("../data/people.csv")
+    train = pd.read_csv("../data/act_train.csv")
+    test = pd.read_csv("../data/act_test.csv")
+
+    # Join people to the other data sets
+    train = train.join(people, on="people_id", lsuffix="_act", rsuffix="_people")
+    test = test.join(people, on="people_id", lsuffix="_act", rsuffix="_people")
+
+    return train, test
+
 def peopleprep():
 
     # Read in the people dataset
